c1f928fe6bd6fb64826bfc8e17990605e34d6ba8
Jiandan <jiandcai@emeal.nttdata.com>
2023-12-27 13:22:49 +0100

    Primer commit

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,40 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div
+    class="entry-container mb-3 pointer p-2"
+    @click="$router.push({ name: 'entry', params: { id: 10 }})"
+  >
+    <!-- Titulo -->
+    <div class="entry-title d-flex">
+        <span class="text-success fs-5 fw-bold">15</span>
+        <span class="mx-1 fs-5">Julio</span>
+        <span class="mx-2 fw-light">2021, jueves</span>
+    </div>
+    
+    <div class="entry-description">
+        Deserunt Lorem amet aute velit aliquip consequat eiusmod sint. Irure velit veniam veniam exercitation deserunt excepteur. Sit dolore exercitation voluptate in sit sint. Aliqua minim aliquip et proident cillum eiusmod anim voluptate aute sit ut cillum occaecat nostrud. Cupidatat sit nulla eu sint nulla amet mollit quis ut enim culpa voluptate ut.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.entry-container {
+    border-bottom: 1px solid #2c3e50;
+    transition: 0.2s all ease-in;
+
+    &:hover {
+        background-color: lighten($color: grey, $amount: 45);
+        transition: 0.2s all ease-in;
+    }
+
+    .entry-description {
+        font-size: 12px;
+    }
+}
+</style>
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+    isLoading: true, 
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Ipsum anim ea enim fugiat nulla culpa incididunt sunt enim esse velit nulla exercitation. Sit in ullamco Lorem ad proident deserunt sint adipisicing. Qui minim commodo aliqua aute labore exercitation. In nostrud eu qui mollit sit laborum eu mollit sunt do sint. Et Lorem fugiat mollit qui sit in laborum laborum.',
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'Labore irure ad sunt ex deserunt sunt in culpa mollit quis minim. Dolore cupidatat nostrud veniam eu velit quis est nisi. Dolore consectetur minim do culpa nisi do sit mollit nostrud irure consequat.',
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Magna reprehenderit nisi dolore in culpa. Ad proident est cillum officia ut. Consequat eiusmod proident eu Lorem incididunt labore laboris amet do ut. Fugiat nisi qui commodo in occaecat aliquip. Esse velit sunt pariatur aliqua anim dolor irure ut do deserunt. Cillum dolore Lorem occaecat irure ad laborum cillum adipisicing ex reprehenderit anim ex sunt fugiat. Sint velit consequat excepteur laborum aute commodo officia.',
+            picture: null
+        }
+    ]
+})
